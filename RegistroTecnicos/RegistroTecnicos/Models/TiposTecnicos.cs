b1734b2cd6037b68692752dbd5cbeb401c7e2e1f
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-05-18 11:27:41 -0400

    Modificando el models con TiposTecnicos

---
+++ RegistroTecnicos/RegistroTecnicos/Models/TiposTecnicos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class TiposTecnicos
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "Favor de introducir una descripción.")]
+    public string Descripcion { get; set; }
+}
