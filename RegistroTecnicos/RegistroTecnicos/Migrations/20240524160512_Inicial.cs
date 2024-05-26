868a6cba2045e35d5cd7bdfc080c905035d62885
jairo camilo acosta <jairo_camilo@ucne.edu.do>
2024-05-26 06:23:47 -0400

    Creando nueva migración

---
+++ RegistroTecnicos/RegistroTecnicos/Migrations/20240524160512_Inicial.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Incentivos",
+                columns: table => new
+                {
+                    IncentivoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TecnicoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    CantidadServicios = table.Column<int>(type: "INTEGER", nullable: false),
+                    Monto = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incentivos", x => x.IncentivoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposTecnicos",
+                columns: table => new
+                {
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    IncentivoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposTecnicos", x => x.TipoId);
+                    table.ForeignKey(
+                        name: "FK_TiposTecnicos_Incentivos_IncentivoId",
+                        column: x => x.IncentivoId,
+                        principalTable: "Incentivos",
+                        principalColumn: "IncentivoId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tecnicos",
+                columns: table => new
+                {
+                    TecnicoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(type: "TEXT", nullable: false),
+                    SueldoHora = table.Column<decimal>(type: "TEXT", nullable: false),
+                    TipoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnicos", x => x.TecnicoId);
+                    table.ForeignKey(
+                        name: "FK_Tecnicos_TiposTecnicos_TipoId",
+                        column: x => x.TipoId,
+                        principalTable: "TiposTecnicos",
+                        principalColumn: "TipoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Incentivos_TecnicoId",
+                table: "Incentivos",
+                column: "TecnicoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tecnicos_TipoId",
+                table: "Tecnicos",
+                column: "TipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TiposTecnicos_IncentivoId",
+                table: "TiposTecnicos",
+                column: "IncentivoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Incentivos_Tecnicos_TecnicoId",
+                table: "Incentivos",
+                column: "TecnicoId",
+                principalTable: "Tecnicos",
+                principalColumn: "TecnicoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incentivos_Tecnicos_TecnicoId",
+                table: "Incentivos");
+
+            migrationBuilder.DropTable(
+                name: "Tecnicos");
+
+            migrationBuilder.DropTable(
+                name: "TiposTecnicos");
+
+            migrationBuilder.DropTable(
+                name: "Incentivos");
+        }
+    }
+}
